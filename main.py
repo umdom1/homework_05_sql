574c710e580b84cd31184793daeeac45df1cf126
Denis Denisov <umdom1@gmail.com>
2023-07-19 23:09:26 +0300

    second commit

--- main.py
+++ main.py
@@ -21,18 +21,33 @@ def create_structure():
 def create_new_person(person_id, name, surname, email):
     with db_conn.cursor() as cur:
         cur.execute("INSERT INTO person VALUES (%s, %s, %s, %s);", (person_id, name, surname, email))
+    print('Новые данные добавленны')
     db_conn.commit()
 
 
 def create_new_phone(phone_id, phone, person_id):
     with db_conn.cursor() as cur:
         cur.execute("INSERT INTO phone VALUES (%s, %s, %s);", (phone_id, phone, person_id))
+    print('Телефон дбавлен')
     db_conn.commit()
 
 
-def update_person(name, surname, email, person_id):
+def update_person():
     with db_conn.cursor() as cur:
-        cur.execute("UPDATE person SET person_name=%s, person_surname=%s, person_email=%s WHERE person_id=%s;", (name, surname, email, person_id))
+        update_dict = {}
+        update_id = int(input('Введите person_id: '))
+        name = str(input('Если хотите обновить "Имя" введите его, для продолжения нажмите enter: '))
+        if name != '':
+            update_dict['person_name'] = name
+        surname = str(input('Если хотите обновить "Фамилию" введите его, для продолжения нажмите enter: '))
+        if surname != '':
+            update_dict['person_surname'] = surname
+        email = str(input('Если хотите обновить "email" введите его, для продолжения нажмите enter: '))
+        if email != '':
+            update_dict['person_email'] = email
+        for key, value in update_dict.items():
+            cur.execute('UPDATE person SET "%d" = "%d" WHERE person_id=%d;', (key, value, update_id))
+    print('Данные обновленны')
     db_conn.commit()
 
 
@@ -43,45 +58,47 @@ def delete_phone(phone_id):
 
 def delete_person(person_id):
     with db_conn.cursor() as cur:
-        cur.execute("DELETE FROM person WHERE person_id=%s;", (person_id))
+        cur.execute("SELECT phone_number FROM phone WHERE person_id=%s", (person_id))
+        result = cur.fetchall()
+        if len(result) == 0:
+            cur.execute("DELETE FROM person WHERE person_id=%s;", (person_id))
+        else:
+            cur.execute("DELETE FROM phone WHERE person_id=%s", (person_id))
+            cur.execute("DELETE FROM person WHERE person_id=%s;", (person_id))
+    print(f'Данные по person_id: {person_id} удалены из базы данных')
     db_conn.commit()
 
 
 def search_person():
     with db_conn.cursor() as cur:
-        data = int(input('Выберите критерий для поиска.\n[1] -- Имя\n[2] -- Фамилия\n[3] -- email\n[4] -- Телефон: '))
+        search_dict = {}
+        name = str(input('Чтобы включить в поиск "Имя" введите его, для продолжения нажмите enter: '))
+        if name != '':
+            search_dict['person_name'] = name
+        surname = str(input('Чтобы включить в поиск  "Фамилию" введите его, для продолжения нажмите enter: '))
+        if surname != '':
+            search_dict['person_surname'] = surname
+        email = str(input('Чтобы включить в поиск  "email" введите его, для продолжения нажмите enter: '))
+        if email != '':
+            search_dict['person_email'] = email
+        number = str(input('Чтобы включить в поиск  "телефон" введите его, для продолжения нажмите enter: '))
+        if number != '':
+            search_dict['phone_number'] = number
 
-        if data == 1:
-            name = str(input('Введите имя для поиска: '))
-            cur.execute(
-                    "SELECT person_name, person_surname, person_email, phone_number "
-                    "FROM person "
-                    "INNER JOIN phone on person.person_id = phone.person_id "
-                    "WHERE person_name=%s;", [name])
-        elif data == 2:
-            surname = str(input('Введите фамилию для поиска: '))
-            cur.execute(
-                    "SELECT person_name, person_surname, person_email, phone_number "
-                    "FROM person "
-                    "INNER JOIN phone on person.person_id = phone.person_id "
-                    "WHERE person_surname=%s;", [surname])
-        elif data == 3:
-            email = str(input('Введите email для поиска: '))
-            cur.execute(
-                    "SELECT person_name, person_surname, person_email, phone_number "
-                    "FROM person "
-                    "INNER JOIN phone on person.person_id = phone.person_id "
-                    "WHERE person_email=%s;", [email])
-        elif data == 4:
-            phone = str(input('Введите телефон для поиска: '))
-            cur.execute(
-                    "SELECT person_name, person_surname, person_email, phone_number "
-                    "FROM person "
-                    "INNER JOIN phone on person.person_id = phone.person_id "
-                    "WHERE phone_number=%s;", [phone])
+        search_list = []
+        for key, value in search_dict.items():
+            e = str(key + " = " + "'" + value + "'")
+            search_list.append(e)
+
+        date = " AND ".join(search_list)
+        sql = "SELECT person_name, person_surname, person_email, phone_number " \
+              "FROM person " \
+              "LEFT JOIN phone on person.person_id = phone.person_id " \
+              "WHERE " + (date) + ";"
+
+        cur.execute(sql)
+
         result = cur.fetchall()
         print(result)
-
-search_person()
 
 
